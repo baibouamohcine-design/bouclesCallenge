15175bf5eaa8f9309e846ff74820ca5a2de6c518
unknown <baibouamohamed@gmail.com>
2026-09-18 11:36:26 +0200

    challenege done

---
+++ challenge1.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let nom = Number(prompt("entrer a nombre : "))
+for (let i = 1  ; i <= 10; i++) {
+    let rs =  nom * i
+  console.log(nom , " * ", i , " = " ,rs )
+}
+
+
+
